f89c8382b7b9a2908c50f87ea6d98c82b475301d
Vincent Umelue <vincent.umelue@nhs.net>
2025-01-06 02:41:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+A simple hello world script
+"""
+
+def print_hi(first_name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {first_name}')  # Press F9 to toggle the breakpoint.
+
+def print_surname(surname):
+    print(f'Hi, {surname}')
+
+
+
+# if __name__ == '__main__':
+
+print_hi('PyCharm')
+print_surname('IDE')
+
